2a247180026d69cd6be1fe71e46423ec3ecac663
zshadow19 <z.shadow19@gmail.com>
2015-05-28 23:57:25 +0600

    Yolo

--- src/com/communityblogproject/SigninServlet.java
+++ src/com/communityblogproject/SigninServlet.java
@@ -2,6 +2,7 @@ package com.communityblogproject;
 
 import java.io.IOException;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,12 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 
 public class SigninServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
-		
+		jdbc data=new jdbc();
+		if(password.equals(data.query("user","UserPassword","UserEmail",email)))
+		{
+			System.out.println("Matched");
+		}
 		System.out.println(email);
 		System.out.println(password);
 		response.sendRedirect("home");
