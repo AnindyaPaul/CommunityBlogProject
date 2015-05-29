c8f574f076eaacba8fa967f3e52f315164e627be
zshadow19 <z.shadow19@gmail.com>
2015-05-30 05:04:51 +0600

    follow

---
+++ src/com/communityblogproject/Follow.java
@@ -0,0 +1,21 @@
+package com.communityblogproject;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Follow extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		User user=(User)request.getSession().getAttribute("User");
+		String UserId=user.getUserId();
+		String followId=request.getParameter("followID");
+		jdbc data=new jdbc();
+		data.newFollowRow(UserId,followId);
+		data.close();
+	}
+
+}
